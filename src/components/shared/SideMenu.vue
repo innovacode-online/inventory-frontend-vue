29a03393f2ef2e56c5a68283d8bb0c70bae716e8
Ale <duartemattias4@gmail.com>
2023-11-06 21:38:40 -0400

    adding side menu

---
+++ src/components/shared/SideMenu.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { useAuthStore } from '../../stores/auth';
+const authStore = useAuthStore();
+
+</script>
+
+<template>
+    <nav class="sidemenu flex flex-col">
+        <h1 class="mb-10 text-xl">Nombre del usuario</h1>
+        
+        <ul>
+            <li>
+                <RouterLink :to="{name: 'profile'}" class="nav__menu--item">
+                    <i class="uil uil-apps"></i>
+                    Perfil
+                </RouterLink>
+            </li>
+        </ul>
+    
+    
+    
+    </nav>
+
+</template>
--- src/layouts/root/RooLayout.vue
+++ src/layouts/root/RooLayout.vue
@@ -1,18 +1,21 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router';
 import Toast from '@/components/shared/Toast.vue';
-
+import SideMenu from '@/components/shared/SideMenu.vue';
 
 </script>
 
 <template>
-    <!-- LOADING VIEW -->
+    <div>
 
-
-    <!-- SIDEMENU -->
-
-
-    <!-- CONTENIDO PRINCIPAL -->
-    <Toast/>
-    <RouterView />
+        <!-- LOADING VIEW -->
+        
+        <main class="flex ">
+            <SideMenu/>
+            <div class="w-full px-10 pt-6 h-screen md:overflow-y-scroll overflow-x-hidden relative">
+                <Toast/>
+                <RouterView />
+            </div>
+        </main>        
+    </div>
 </template>
